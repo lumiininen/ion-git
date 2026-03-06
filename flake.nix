1612fc25b883e9a64549eb863891bf4d61a734c2
Lumiini <lumiini@proton.me>
2026-03-07 00:28:11 +0200

    fix acc. deleted copywrite

--- flake.nix
+++ flake.nix
@@ -1,3 +1,9 @@
+# Copyright (c) Lumiini
+# All rights reserved.
+#
+# This file is licensed under the MIT license (found in the
+# LICENSE file in the root directory of this source tree).
+
 {
   description = "Ion shell from latest master";
 
